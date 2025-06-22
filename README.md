658bb1f82144aae249aff3dd262774ead14b11f1
Radzivon Bartoshyk <radzivon.bartoshyk@proton.me>
2025-06-22 16:45:24 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple libm
+
+Mostly fast and accurate math.
---
+++ src/lib.rs
@@ -0,0 +1,117 @@
+/*
+ * // Copyright (c) Radzivon Bartoshyk 6/2025. All rights reserved.
+ * //
+ * // Redistribution and use in source and binary forms, with or without modification,
+ * // are permitted provided that the following conditions are met:
+ * //
+ * // 1.  Redistributions of source code must retain the above copyright notice, this
+ * // list of conditions and the following disclaimer.
+ * //
+ * // 2.  Redistributions in binary form must reproduce the above copyright notice,
+ * // this list of conditions and the following disclaimer in the documentation
+ * // and/or other materials provided with the distribution.
+ * //
+ * // 3.  Neither the name of the copyright holder nor the names of its
+ * // contributors may be used to endorse or promote products derived from
+ * // this software without specific prior written permission.
+ * //
+ * // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * // AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * // IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * // DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * // FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * // DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * // SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * // CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#![forbid(unsafe_code)]
+#![deny(unreachable_pub)]
+#![allow(
+    clippy::excessive_precision,
+    clippy::approx_constant,
+    clippy::manual_range_contains
+)]
+mod acos;
+mod acosf;
+mod asin;
+mod asinf;
+mod atan;
+mod atan2;
+mod atan2f;
+mod atanf;
+mod cbrt;
+mod cbrtf;
+mod common;
+mod cosf;
+mod coshf;
+mod dekker;
+mod exp;
+mod exp10;
+mod exp10f;
+mod exp2;
+mod exp2f;
+mod expf;
+mod floor;
+mod hypot;
+mod j1;
+mod log;
+mod log10;
+mod log10f;
+mod log2;
+mod log2f;
+mod logf;
+mod pow;
+mod powf;
+mod sin;
+mod sincos;
+mod sincosf;
+mod sinf;
+mod sinhf;
+mod sqrtf;
+mod tan;
+mod tanf;
+mod tanhf;
+
+pub use acos::f_acos;
+pub use acosf::f_acosf;
+pub use asin::f_asin;
+pub use asinf::f_asinf;
+pub use atan::f_atan;
+pub use atan2::f_atan2;
+pub use atan2f::f_atan2f;
+pub use atanf::f_atanf;
+pub use cbrt::f_cbrt;
+pub use cbrtf::{cbrtf, f_cbrtf};
+pub use common::{copysignfk, copysignk};
+pub use cosf::f_cosf;
+pub use coshf::f_coshf;
+pub use exp::{exp, f_exp};
+pub use exp2::f_exp2;
+pub use exp2f::f_exp2f;
+pub use exp10::f_exp10;
+pub use exp10f::f_exp10f;
+pub use expf::{expf, f_expf};
+pub use floor::{floor, floorf};
+pub use hypot::{f_hypot3f, f_hypotf};
+pub use j1::f_j1;
+pub use log::{f_log, log};
+pub use log2::f_log2;
+pub use log2f::f_log2f;
+pub use log10::f_log10;
+pub use log10f::f_log10f;
+pub use logf::f_logf;
+pub use logf::logf;
+pub use pow::{f_pow, pow};
+pub use powf::{dirty_powf, f_powf, powf};
+pub use sin::f_cos;
+pub use sin::f_sin;
+pub use sincos::f_sincos;
+pub use sincosf::f_sincosf;
+pub use sinf::f_sinf;
+pub use sinhf::f_sinhf;
+pub use sqrtf::sqrtf;
+pub use tan::f_tan;
+pub use tanf::f_tanf;
+pub use tanhf::f_tanhf;
