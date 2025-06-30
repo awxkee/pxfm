d2a427fa8f988f47335d1588a2ba334fdc7c4d7f
Radzivon Bartoshyk <radzivon.bartoshyk@proton.me>
2025-06-30 11:35:47 +0100

    Added log2p1 ULP 0.5

--- app/benches/math/main.rs
+++ app/benches/math/main.rs
@@ -10,12 +10,21 @@ use pxfm::{
     f_atan2, f_atan2f, f_atan2pif, f_atanf, f_atanpi, f_atanpif, f_cbrt, f_cbrtf, f_cos, f_cosf,
     f_coshf, f_cospi, f_cospif, f_exp, f_exp2, f_exp2f, f_exp2m1, f_exp2m1f, f_exp10, f_exp10f,
     f_exp10m1, f_exp10m1f, f_expf, f_expm1, f_expm1f, f_j1, f_log, f_log1p, f_log1pf, f_log2,
-    f_log2f, f_log2p1f, f_log10, f_log10f, f_log10p1f, f_logf, f_pow, f_powf, f_sin, f_sincos,
-    f_sincosf, f_sinf, f_sinhf, f_sinpi, f_sinpif, f_tan, f_tanf, f_tanhf, f_tanpi, f_tanpif, powf,
+    f_log2f, f_log2p1, f_log2p1f, f_log10, f_log10f, f_log10p1f, f_logf, f_pow, f_powf, f_sin,
+    f_sincos, f_sincosf, f_sinf, f_sinhf, f_sinpi, f_sinpif, f_tan, f_tanf, f_tanhf, f_tanpi,
+    f_tanpif, powf,
 };
 use std::hint::black_box;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("pxfm: f_log2p1", |b| {
+        b.iter(|| {
+            for i in 1..1000 {
+                black_box(f_log2p1(i as f64 / 1000.0));
+            }
+        })
+    });
+
     c.bench_function("system: ln_1p", |b| {
         b.iter(|| {
             for i in 1..1000 {
