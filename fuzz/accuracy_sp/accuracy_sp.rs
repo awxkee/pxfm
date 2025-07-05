665fac2ee70c8b443f7edf3592e05166e610307e
Radzivon Bartoshyk <radzivon.bartoshyk@proton.me>
2025-07-05 17:47:01 +0100

    Added erfcf ULP 0.5

--- fuzz/accuracy_sp/accuracy_sp.rs
+++ fuzz/accuracy_sp/accuracy_sp.rs
@@ -3,9 +3,9 @@
 use libfuzzer_sys::fuzz_target;
 use pxfm::{
     f_acosf, f_acoshf, f_acospif, f_asinf, f_asinh, f_asinhf, f_asinpif, f_atan2f, f_atan2pif,
-    f_atanhf, f_atanpif, f_cbrtf, f_cosf, f_coshf, f_cospif, f_erff, f_exp2f, f_exp2m1f, f_exp10f,
-    f_exp10m1f, f_expf, f_expm1f, f_hypotf, f_log1pf, f_log2f, f_log2p1f, f_log10f, f_log10p1f,
-    f_logf, f_powf, f_sincf, f_sinf, f_sinhf, f_sinpif, f_tanf, f_tanhf, f_tanpif,
+    f_atanhf, f_atanpif, f_cbrtf, f_cosf, f_coshf, f_cospif, f_erfcf, f_erff, f_exp2f, f_exp2m1f,
+    f_exp10f, f_exp10m1f, f_expf, f_expm1f, f_hypotf, f_log1pf, f_log2f, f_log2p1f, f_log10f,
+    f_log10p1f, f_logf, f_powf, f_sincf, f_sinf, f_sinhf, f_sinpif, f_tanf, f_tanhf, f_tanpif,
 };
 use rug::ops::Pow;
 use rug::{Assign, Float};
@@ -119,14 +119,15 @@ fuzz_target!(|data: (f32, f32)| {
     let x1 = data.0;
     let mpfr_x0 = Float::with_val(70, x0);
     let mpfr_x1 = Float::with_val(70, x1);
-    let sinc_x0 = if x0 == 0. {
-        Float::with_val(70, 1.)
-    } else {
-        mpfr_x0.clone().sin().div(&mpfr_x0)
-    };
 
+    test_method(x0, f_erfcf, &mpfr_x0.clone().erfc(), "f_erfcf".to_string());
     test_method(x0, f_erff, &mpfr_x0.clone().erf(), "f_erff".to_string());
     if x0.abs() < 6.60 {
+        let sinc_x0 = if x0 == 0. {
+            Float::with_val(70, 1.)
+        } else {
+            mpfr_x0.clone().sin().div(&mpfr_x0)
+        };
         test_method_max_ulp(x0, f_sincf, &sinc_x0, "f_sincf".to_string(), 0.5000);
     }
     test_method_2vals_ignore_nan(
